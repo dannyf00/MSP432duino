b353d611965e4b2b857c6873e22068006b278ee1
dannyf00 <fdan00@live.com>
2017-06-09 16:21:48 -0400

    Create user-code.c

--- user-code.c
+++ user-code.c
@@ -1,7 +1,7 @@
 #include "armduino.h"							//include ARMduino defs
 
-#define LED			10							//LED attached to pin 8/9/10 (=P2.0RED/1GREEN/2BLUE on TI MSP432 Launchpad)
-#define LED_DLY		50							//waste some time, in ms
+#define LED			10					//LED attached to pin 8/9/10 (=P2.0RED/1GREEN/2BLUE on TI MSP432 Launchpad)
+#define LED_DLY			50					//waste some time, in ms
 
 //user setup
 void setup(void) {
@@ -10,6 +10,6 @@ void setup(void) {
 
 //user loop
 void loop(void) {
-	digitalWrite(LED, !digitalRead(LED));		//flip LED
-	delay(LED_DLY);								//waste some time
+	digitalWrite(LED, !digitalRead(LED));				//flip LED
+	delay(LED_DLY);							//waste some time
 }
